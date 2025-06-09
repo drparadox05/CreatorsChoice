0fac905395d351044a1851da982f369c4659bfe9
drparadox05 <deepakshah9016@gmail.com>
2025-06-10 02:28:30 +0530

    fix vercel deployments

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -8,11 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path}`);
-  console.log("-------------------------");
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(`${req.method} ${req.path}`);
+//   console.log("-------------------------");
+//   next();
+// });
 
 app.get('/', (req, res) => {
   res.send('Backend is running!');
