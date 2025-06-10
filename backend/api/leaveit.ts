00c031d77d1f3f27d0e45fcb1f2dc4e553107c7b
drparadox05 <deepakshah9016@gmail.com>
2025-06-10 12:44:27 +0530

    test-4

---
+++ backend/api/leaveit.ts
@@ -0,0 +1,28 @@
+// import express from "express";
+// import serverless from "serverless-http";
+// import cors from 'cors';
+// // import userRouter from "../src/routers/user";  
+// // import workerRouter from "../src/routers/worker";
+
+// console.log("Creating Express app...");
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// // app.use((req, res, next) => {
+// //   console.log(`${req.method} ${req.path}`);
+// //   console.log("-------------------------");
+// //   next();
+// // });
+
+
+// app.get('/ping', (req, res) => {
+//   console.log("Route hit!");
+//   res.send('pong');
+// });
+
+// // app.use("/v1/user", userRouter);
+// // app.use("/v1/worker", workerRouter);
+
+// console.log("App configured, exporting...");
+// export default serverless(app);
