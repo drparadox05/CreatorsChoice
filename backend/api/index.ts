fa421f37b58b59a7ee1edce3d732341e990a8caf
drparadox05 <deepakshah9016@gmail.com>
2025-06-10 10:58:24 +0530

    fixed bug

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -8,17 +8,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path}`);
-  console.log("-------------------------");
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(`${req.method} ${req.path}`);
+//   console.log("-------------------------");
+//   next();
+// });
 
 app.get('/', (req, res) => {
+  console.log("Route hit!");
   res.send('Backend is running!');
 });
 
 // app.use("/v1/user", userRouter);
 // app.use("/v1/worker", workerRouter);
 
+console.log("App configured, exporting...");
 export default serverless(app);
