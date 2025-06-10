cd3e2ce926b491e729b36c9a2cfee307161104bc
drparadox05 <deepakshah9016@gmail.com>
2025-06-10 11:12:16 +0530

    test

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -4,9 +4,9 @@ import cors from 'cors';
 // import userRouter from "../src/routers/user";  
 // import workerRouter from "../src/routers/worker";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
 
 // app.use((req, res, next) => {
 //   console.log(`${req.method} ${req.path}`);
@@ -14,13 +14,17 @@ app.use(express.json());
 //   next();
 // });
 
-app.get('/', (req, res) => {
-  console.log("Route hit!");
-  res.send('Backend is running!');
-});
+export default function handler(req: any, res: any) {
+  res.status(200).json({ message: 'Express test working' });
+}
+
+// app.get('/', (req, res) => {
+//   console.log("Route hit!");
+//   res.send('Backend is running!');
+// });
 
 // app.use("/v1/user", userRouter);
 // app.use("/v1/worker", workerRouter);
 
 console.log("App configured, exporting...");
-export default serverless(app);
+// export default serverless(app);
