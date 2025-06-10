bd2989d7a5e3050fe3fc6cc59098cde186756b6f
drparadox05 <deepakshah9016@gmail.com>
2025-06-10 18:38:03 +0530

    fix

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import serverless from "serverless-http";
 import cors from 'cors';
-// import userRouter from "../src/routers/user";  
-// import workerRouter from "../src/routers/worker";
+import userRouter from "../src/routers/user";  
+import workerRouter from "../src/routers/worker";
 
 console.log("Creating Express app...");
 const app = express();
@@ -18,26 +18,20 @@ app.use((req, res, next) => {
 
 app.get('/ping', (req, res) => {
   console.log("ping Route hit!");
-  res.send('pong');
+  res.status(200).json({ message: 'ping endpoint working' });
+//   res.send('pong');
 });
 
 
 app.get("/", (req, res) => {
   console.log("Route hit!");
-  res.send("Backend is running!");
+  res.status(200).json({ message: 'Backend working' });
+  //   res.send("Backend is running!");
 });
 
 
-// app.use("/v1/user", userRouter);
-// app.use("/v1/worker", workerRouter);
+app.use("/v1/user", userRouter);
+app.use("/v1/worker", workerRouter);
 
 console.log("App configured, exporting...");
-
-if (process.env.NODE_ENV !== "production") {
-  const port = 3000;
-  app.listen(port, () => {
-    console.log(`Local dev server running at http://localhost:${port}`);
-  });
-}
-
 export default serverless(app);
